2b82e7aac0c4f87d6ac72867195a45f1b0d804c5
unknown <koalakid154@gmail.com>
2020-03-05 17:49:35 +0900

    2일차

---
+++ baekjoon C/baekjoon if/baekjoon C/2753.c
@@ -0,0 +1,19 @@
+////À±³â ¸ÂÃß±â
+//#include<stdio.h>
+//
+//int main() {
+//
+//	int a;
+//
+//	scanf_s("%d", &a);
+//
+//	if (a % 4 == 0)
+//	{
+//		if (a % 100 == 0) {
+//			if (a % 400 == 0) printf("1");
+//			else printf("0");
+//		}
+//		else printf("1");
+//	}
+//	else printf("0");
+//}
---
+++ baekjoon C/baekjoon if/baekjoon C/5543.c
@@ -0,0 +1,24 @@
+//// 3개의 햄버거와 2개의 음료로 세트를 만들면 뭐가 가장 쌀까?
+//#include<stdio.h>
+//
+//int main() {
+//
+//	int a, b, c, d, e;
+//	int f, g;
+//
+//	scanf_s("%d", &a);
+//	scanf_s("%d", &b);
+//	scanf_s("%d", &c);
+//	scanf_s("%d", &d);
+//	scanf_s("%d", &e);
+//
+//	if (a <= b && a <= c) f = a;
+//	else if (b <= a && b <= c) f = b;
+//	else f = c;
+//
+//	if (d <= e) g = d;
+//	else g = e;
+//
+//	printf("%d", f + g - 50);
+//
+//}
